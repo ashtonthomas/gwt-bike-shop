4ba40a04d888e64219eaaf7d1815baf80d0a0713
Ashton Thomas <athomas5@enova.com>
2014-10-27 20:27:47 -0500

    adding edit for bike widget

--- src/main/java/com/biker/client/main/widgets/BikeWidget.java
+++ src/main/java/com/biker/client/main/widgets/BikeWidget.java
@@ -1,11 +1,15 @@
 package com.biker.client.main.widgets;
 
+import com.biker.client.common.fast.FastPress;
+import com.biker.client.common.fast.PressEvent;
+import com.biker.client.common.fast.PressHandler;
 import com.biker.client.common.resource.CoreClientBundle;
 import com.biker.client.common.resource.CoreStyle;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
@@ -20,8 +24,21 @@ public class BikeWidget extends Composite {
 	
 	@UiField(provided=true)
 	CoreStyle css = CoreClientBundle.INSTANCE.coreStyle();
+	
+	@UiField
+	FastPress edit;
+	
+	@UiField
+	HTMLPanel edit_panel;
+	
+	@UiField
+	HTMLPanel edit_show;
+	
+	@UiField
+	HTMLPanel edit_hide;
 
 	private final Integer bikeId;
+	private boolean isEditShowing = false;
 	
 	@AssistedInject
 	public BikeWidget(@Assisted("bikeId") Integer bikeId) {
@@ -32,7 +49,19 @@ public class BikeWidget extends Composite {
 	}
 	
 	private void setup(){
-		// setup click handlers and all that jazz
+		edit.addPressHandler(new PressHandler() {
+			@Override
+			public void onPress(PressEvent event) {
+				toggleEdit(!isEditShowing);
+			}
+		});
+	}
+	
+	private void toggleEdit(boolean show){
+		isEditShowing = show;
+		edit_panel.setVisible(isEditShowing);
+		edit_hide.setVisible(isEditShowing);
+		edit_show.setVisible(!isEditShowing);
 	}
 	
 	private void refresh(){
