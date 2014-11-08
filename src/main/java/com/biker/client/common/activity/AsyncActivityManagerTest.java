0a83379047ebb5c4ad51c10212d3969d90765f9f
Ashton Thomas <athomas5@enova.com>
2014-11-08 16:36:23 -0600

    format and playing with tests

---
+++ src/main/java/com/biker/client/common/activity/AsyncActivityManagerTest.java
@@ -0,0 +1,15 @@
+package com.biker.client.common.activity;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class AsyncActivityManagerTest extends TestCase {
+
+  @Test
+  public void test() {
+    // fail("Not yet implemented");
+  }
+
+}
